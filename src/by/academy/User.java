99d08019083c451e31e2701bbc55b3ca9263e64f
maddx2020 <madd.xx83@gmail.com>
2020-07-22 02:49:29 +0300

    mass remove

---
+++ src/by/academy/User.java
@@ -0,0 +1,25 @@
+package by.academy;
+
+public class User {
+	private String name;
+
+	public User() {
+		super();
+
+	}
+
+	public User(String name) {
+		super();
+		this.name = name;
+
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
