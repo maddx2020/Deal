6daa1342588b72d506ed26c950592adea84dd571
maddx2020 <madd.xx83@gmail.com>
2020-07-22 19:34:46 +0300

    +

---
+++ src/by/academy/ProduserInfo.java
@@ -0,0 +1,15 @@
+package by.academy;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Target;
+
+@Target({})
+@Inherited
+public @interface ProduserInfo {
+	
+	String name();
+	String company();
+	String[] prodyctTypes();
+	int partNumber();
+}
